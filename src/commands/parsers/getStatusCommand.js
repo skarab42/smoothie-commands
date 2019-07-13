619162c768cbad067d9814529fbc23762b7e8f1c
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-07-13 09:47:43 +0200

    typo

--- src/commands/parsers/getStatusCommand.js
+++ src/commands/parsers/getStatusCommand.js
@@ -1,3 +1,4 @@
+// https://github.com/Smoothieware/Smoothieware/blob/9e5477518b1c85498a68e81be894faea45d6edca/src/modules/utils/simpleshell/SimpleShell.cpp#L887
 import { UNKNOWN_RESPONSE_ERROR } from '../error-types.js'
 import CommandError from '../CommandError.js'
 
