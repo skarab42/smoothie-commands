3297134855a76a2c61ad5b2bc444ee92a0be8ebd
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-07-13 11:02:11 +0200

    typo

--- src/commands/parsers/mvCommand.js
+++ src/commands/parsers/mvCommand.js
@@ -1,3 +1,4 @@
+// https://github.com/Smoothieware/Smoothieware/blob/9e5477518b1c85498a68e81be894faea45d6edca/src/modules/utils/simpleshell/SimpleShell.cpp#L356
 import { COULD_NOT_RENAME_ERROR } from '../error-types.js'
 import CommandError from '../CommandError.js'
 
