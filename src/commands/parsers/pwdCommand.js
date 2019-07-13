76e30cc7e72b8723977a15872eaeb95ba5d4e0b0
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-07-13 11:14:51 +0200

    typo

--- src/commands/parsers/pwdCommand.js
+++ src/commands/parsers/pwdCommand.js
@@ -1,3 +1,4 @@
+// https://github.com/Smoothieware/Smoothieware/blob/9e5477518b1c85498a68e81be894faea45d6edca/src/modules/utils/simpleshell/SimpleShell.cpp#L390
 const command = 'pwd'
 const usage = 'pwd'
 const description = 'Get (print) current working directory'
