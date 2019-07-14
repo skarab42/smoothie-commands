8a88a498d8e03d85e53ad55af85001141888ae9f
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-07-14 11:09:03 +0200

    typo

--- src/commands/parsers/resetCommand.js
+++ src/commands/parsers/resetCommand.js
@@ -1,7 +1,7 @@
 // https://github.com/Smoothieware/Smoothieware/blob/9e5477518b1c85498a68e81be894faea45d6edca/src/modules/utils/simpleshell/SimpleShell.cpp#L674
 const command = 'reset'
-const usage = 'reset <arg1> [<arg2>]'
-const description = 'Command description...'
+const usage = 'reset'
+const description = 'Reset the board'
 
 function parse ({ args, response }) {
   return { message: response }
